8ea5b0673a4f8c29f7f2b29163fd15b1b51c6645
JhonE17 <40145138+JhonE17@users.noreply.github.com>
2024-02-09 02:43:37 -0500

    Update GifExpertApp.jsx

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { AddCategory, GifGrid } from "./components/";
 
 export const GifExpertApp = () => {
-  const [categories, setCategories] = useState(["Example"]);
+  const [categories, setCategories] = useState(["Nice"]);
 
   const onAddCategory = (newCategory) => {
     if (categories.includes(newCategory)) return;
